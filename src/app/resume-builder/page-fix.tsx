01fc779ae74e947805ab168a21545e852cdd50a8
Maddireddykiran <maddireddykirankumar2117@gmail.com>
2025-04-21 18:31:30 +0530

    resume  commit

--- src/app/resume-builder/page-fix.tsx
+++ src/app/resume-builder/page-fix.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable */
+"use client";
+
 import { Resume } from "components/Resume";
 import React, { useState } from 'react';
-const [activeTab, setActiveTab] = useState<string>('defaultTab');
-{activeTab === "resume" && (
-  <div className="w-full h-full">
-    <Resume />
-  </div>
-)} 
+
+export default function PageFix() {
+  const [activeTab, setActiveTab] = useState<string>('defaultTab');
+  
+  return (
+    <div>
+      {activeTab === "resume" && (
+        <div className="w-full h-full">
+          <Resume />
+        </div>
+      )}
+    </div>
+  );
+}
